ee22aa6b6abca14f2acc3773890c0cca2176895f
Jasleen Kaur <kaurjasleen1728@gmail.com>
2024-11-12 23:58:03 +0530

    added css and js to the weather forecast cards

--- scripts.js
+++ scripts.js
@@ -42,34 +42,82 @@ function updateWeather(city) {
     city.data.temperature.feels_like
   )}`;
 
-//   let windSpeed = document.querySelector(".wind-speed .condition .values");
-//   windSpeed.innerHTML = `${city.data.wind.speed}km/h`;
+  //   let windSpeed = document.querySelector(".wind-speed .condition .values");
+  //   windSpeed.innerHTML = `${city.data.wind.speed}km/h`;
 
-//   let humidity = document.querySelector(".humidity .condition .values");
-//   humidity.innerHTML = `${city.data.temperature.humidity}%`;
+  //   let humidity = document.querySelector(".humidity .condition .values");
+  //   humidity.innerHTML = `${city.data.temperature.humidity}%`;
 
   let weather_description = document.querySelector(".description");
   weather_description.innerHTML = `${city.data.condition.description}`;
   let weather_icon = document.querySelector(".current-city-weather-icon");
   weather_icon.innerHTML = `<img src="${city.data.condition.icon_url}"
-                        alt="few-clouds-day">`;
+                        alt="${city.data.condition.icon}">`;
 
   formatDate();
 }
 
-// function update_min_max_temp(city) {
-//   let minimum_temp = document.querySelector(
-//     ".min-temperature .condition .values"
-//   );
-//   let maximum_temp = document.querySelector(
-//     ".max-temperature .condition .values"
-//   );
+function updateCards(city) {
+  let weatherForecastContainer = document.querySelector(
+    ".weather-forecast-container"
+  );
 
-//   // console.log(minimum_temp);
-//   // console.log(maximum_temp);
-//   // minimum_temp.innerHTML = `${city.data.daily.temperature.minimum}`;
-//   // maximum_temp.innerHTML = `${city.data.daily.temperature.maximum}`;
-// }
+  let today = new Date();
+  let day = today.getDay();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let date = today.getDate();
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  let month = today.getMonth();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "November",
+    "October",
+    "December",
+  ];
+
+  const totalCards = 5;
+  weatherForecastContainer.innerHTML = "";
+  for (i = 0; i < totalCards; i++) {
+    ++date;
+    weatherForecastContainer.innerHTML += `<div class="weather-forecast-cards">
+                <div class="forecast-time">
+                ${days[day]},${date}
+                </div>
+                <div class="forecast-icon">
+                <img src="${city.data.daily[i].condition.icon_url}"
+                alt="${city.data.daily[i].condition.icon}">
+                </div>
+                <div class="forecast-temperatures"><p>
+                <span class="temp-container"><span class="min-temperature temp-update">${Math.round(
+                  city.data.daily[i].temperature.minimum
+                )}</span><span>º</span></span><span class="back-slash">/</span><strong>
+                
+                <span class="temp-container"><span class="max-temperature temp-update">${Math.round(
+                  city.data.daily[i].temperature.maximum
+                )}</span><span>º</span></span></strong>
+                </p></div>
+                </div>`;
+    ++day;
+  }
+}
 
 function searchCity(response) {
   response.preventDefault();
@@ -80,9 +128,9 @@ function searchCity(response) {
   cityName.innerHTML = cityElement;
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityElement}&key=${apiKey}`;
   let foreCastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${cityElement}&key=${apiKey}`;
-  console.log(foreCastApiUrl);
+  // console.log(foreCastApiUrl);
   axios.get(apiUrl).then(updateWeather);
-  axios.get(foreCastApiUrl).then(update_min_max_temp);
+  axios.get(foreCastApiUrl).then(updateCards);
 }
 
 let searchButton = document.querySelector(".input-search-submit");
@@ -114,4 +162,6 @@ let apiKey = "2d8o4b96bdta6ee065c85fc43853285d";
 let cityName = document.querySelector("#city-name");
 cityName.innerHTML = defaultCity;
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${defaultCity}&key=${apiKey}`;
+let foreCastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${defaultCity}&key=${apiKey}`;
 axios.get(apiUrl).then(updateWeather);
+axios.get(foreCastApiUrl).then(updateCards);
